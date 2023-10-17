9930dad91e17c014d2c80fbad2d2b447c772c7df
Mansour Quddus <itsnothisuser@gmail.com>
2023-10-16 23:27:53 -0400

    playing around with UI

--- src/main/java/org/manso/Main.java
+++ src/main/java/org/manso/Main.java
@@ -8,14 +8,21 @@ import org.opencv.videoio.VideoCapture;
 import org.opencv.highgui.HighGui;
 import org.opencv.videoio.Videoio;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private static volatile boolean stopButtonClicked = false;
+    private static volatile boolean exitButtonClicked = false;
 
+
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
     public static void main(String[] args) {
 
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         int CHECKERBOARD_COLS = 4;
         int CHECKERBOARD_ROWS = 6;
         Size boardSize = new Size(CHECKERBOARD_COLS, CHECKERBOARD_ROWS);
@@ -49,13 +56,29 @@ public class Main {
 
         Mat image = new Mat();
         int currentFrame = 0;
+
+        // Button layout
+
+        JButton stopButton = new JButton("Stop Calibration");
+        stopButton.setBounds(10, HighGui.WINDOW_NORMAL - 40, 80, 30);
+        stopButton.addActionListener( e -> stopButtonClicked = true);
+
+        JButton exitButton = new JButton("Quit Program");
+
+        JFrame options = HighGui.createJFrame("Camera Calibration", HighGui.WINDOW_NORMAL);
+        options.setLayout(new FlowLayout());
+        Container optionsPane = options.getContentPane();
+        optionsPane.add(stopButton, BorderLayout.SOUTH);
+        options.setSize(200, 200);
+        options.setVisible(true);
+
         while (true) {
             //frameWidth = capture.get(Videoio.CAP_PROP_FRAME_WIDTH);
             //frameHeight = capture.get(Videoio.CAP_PROP_FRAME_HEIGHT);
             currentFrame +=1;
             System.out.println(currentFrame);
             capture.read(image);
-            if(currentFrame == 50) {break;}
+            //if(currentFrame == 50) {break;}
             Mat gray = new Mat();
             Imgproc.cvtColor(image, gray, Imgproc.COLOR_BGR2GRAY);
             //Imgproc.cvtColor(image, image, Imgproc.COLOR_BGR2GRAY);
